16bf1f8ba8812c6458d285ad0300cd20325a349d
Carlos Munoz Diaz <carlos.munoz@cchs.csic.es>
2022-04-22 12:06:56 +0200

    Cambio datos + Continuación desarrollo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -1,23 +1,30 @@
 //Desarrollo de las visualizaciones
 import * as d3 from 'd3';
-import { numberWithCommas2 } from '../helpers';
-//import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
+import { numberWithCommas3 } from '../helpers';
+import { getInTooltip, getOutTooltip, positionTooltip } from '../modules/tooltip';
 import { setChartHeight } from '../modules/height';
 import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
 //Colores fijos
-const COLOR_PRIMARY_1 = '#F8B05C', 
-COLOR_PRIMARY_2 = '#E37A42',
+const COLOR_PRIMARY_1 = '#F8B05C',
 COLOR_COMP_1 = '#528FAD', 
 COLOR_COMP_2 = '#AADCE0',
-COLOR_GREY_1 = '#D6D6D6', 
-COLOR_GREY_2 = '#A3A3A3',
-COLOR_ANAG__PRIM_1 = '#BA9D5F', 
-COLOR_ANAG_PRIM_2 = '#9E6C51',
-COLOR_ANAG_PRIM_3 = '#9E3515',
+COLOR_GREY_1 = '#D6D6D6',
+COLOR_ANAG_PRIM_1 = '#BA9D5F',
 COLOR_ANAG_COMP_1 = '#1C5A5E';
+let tooltip = d3.select('#tooltip');
+
+//Diccionario
+let dictionary = {
+    causasexternas: 'Causas externas',
+    circulatorio: 'Enf. sist. circulatorio',
+    respiratorio: 'Enf. sist. respiratorio',
+    parasitarias: 'Enf. infecciosas',
+    tumores: 'Tumores',
+    otros: 'Otras causas'
+}
 
 export function initChart(iframe) {
     //Desarrollo del gráfico
@@ -27,7 +34,7 @@ export function initChart(iframe) {
         data = data.filter(function(item) { if(item.Edad != 'Todas las edades'){ return item; } });
 
         //Declaramos fuera las variables genéricas
-        let margin = {top: 20, right: 20, bottom: 40, left: 35},
+        let margin = {top: 10, right: 20, bottom: 40, left: 35},
             width = document.getElementById('bars--first').clientWidth - margin.left - margin.right,
             height = document.getElementById('bars--first').clientHeight - margin.top - margin.bottom;
 
@@ -45,9 +52,8 @@ export function initChart(iframe) {
             .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        let gruposEnfermedadesMujeres = ['causas_externas_mujeres','circulatorio_mujeres','respiratorio_mujeres','parasitarias_mujeres','tumores_mujeres','otros_mujeres'];
-        let gruposEnfermedadesHombres = ['causas_externas_hombres','circulatorio_hombres','respiratorio_hombres','parasitarias_hombres','tumores_hombres','otros_hombres'];
-
+        let gruposEnfermedadesMujeres = ['causasexternas_mujeres','circulatorio_mujeres','respiratorio_mujeres','parasitarias_mujeres','tumores_mujeres','otros_mujeres'];
+        let gruposEnfermedadesHombres = ['causasexternas_hombres','circulatorio_hombres','respiratorio_hombres','parasitarias_hombres','tumores_hombres','otros_hombres'];
         
         //Eje X
         let x = d3.scaleBand()
@@ -64,6 +70,9 @@ export function initChart(iframe) {
                     .attr("dy", ".15em")
                     .attr("transform", "rotate(-45)");
             });
+            
+            g.call(function(g){g.selectAll('.tick line').remove()});
+            g.call(function(g){g.select('.domain').remove()});
         }
         
         chart1.append("g")
@@ -79,22 +88,34 @@ export function initChart(iframe) {
             .domain([0, 100])
             .range([height, 0]);
 
+        let yAxis = function(g) {
+            g.call(d3.axisLeft(y).ticks(5));
+            g.selectAll('.tick line')
+                .attr('class', function(d,i) {
+                    if (d == 0) {
+                        return 'line-special';
+                    }
+                })
+                .attr('x1', '0')
+                .attr('x2', `${width}`);
+        }
+
         chart1.append("g")
             .attr("class", "yaxis")
-            .call(d3.axisLeft(y));
+            .call(yAxis);
 
         chart2.append("g")
             .attr("class", "yaxis")
-            .call(d3.axisLeft(y));
+            .call(yAxis);
 
         //Colores
         let colorMujeres = d3.scaleOrdinal()
             .domain(gruposEnfermedadesMujeres)
-            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_GREY_1, COLOR_GREY_2, COLOR_OTHER_1]);
+            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_ANAG_COMP_1, COLOR_ANAG_PRIM_1, COLOR_GREY_1]);
 
         let colorHombres = d3.scaleOrdinal()
             .domain(gruposEnfermedadesHombres)
-            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_GREY_1, COLOR_GREY_2, COLOR_OTHER_1]);
+            .range([COLOR_PRIMARY_1, COLOR_COMP_2, COLOR_COMP_1, COLOR_ANAG_COMP_1, COLOR_ANAG_PRIM_1, COLOR_GREY_1]);
 
         let dataStackedWomen = d3.stack()
             .keys(gruposEnfermedadesMujeres)
@@ -112,17 +133,26 @@ export function initChart(iframe) {
                 .enter()
                 .append("g")
                 .attr("fill", function(d) { return colorHombres(d.key); })
+                .attr('class', function(d) {
+                    return 'rect-padre-1 ' + d.key;
+                })
                 .selectAll("rect")
                 .data(function(d) { return d; })
                 .enter()
                 .append("rect")
-                    .attr('class','prueba-1')
+                    .attr('class','rect-1')
                     .attr("x", function(d) { return x(d.data.Edad); })
                     .attr("y", function(d) { return y(0); })
                     .attr("height", function(d) { return 0; })
                     .attr("width",x.bandwidth())
+                    .on('mouseover', function(d,i,e) {
+
+                    })
+                    .on('mouseout', function(d,i,e) {
+
+                    })
                     .transition()
-                    .duration(2500)
+                    .duration(2000)
                     .attr("y", function(d) { return y(d[1]); })
                     .attr("height", function(d) { return y(d[0]) - y(d[1]); });
 
@@ -133,23 +163,50 @@ export function initChart(iframe) {
                 .enter()
                 .append("g")
                 .attr("fill", function(d) { return colorMujeres(d.key); })
+                .attr('class', function(d) {
+                    return 'rect-padre-2 ' + d.key;
+                })
                 .selectAll("rect")
                 .data(function(d) { return d; })
                 .enter()
                 .append("rect")
-                    .attr('class','prueba-2')
+                    .attr('class','rect-2')
                     .attr("x", function(d) { return x(d.data.Edad); })
                     .attr("y", function(d) { return y(0); })
                     .attr("height", function(d) { return 0; })
                     .attr("width",x.bandwidth())
+                    .on('mouseover', function(d,i,e) {
+
+                    })
+                    .on('mouseout', function(d,i,e) {
+                        
+                    })
                     .transition()
-                    .duration(2500)
+                    .duration(2000)
                     .attr("y", function(d) { return y(d[1]); })
                     .attr("height", function(d) { return y(d[0]) - y(d[1]); });
         }
 
         function animateChart() {
+            chart1.selectAll('.rect-1')
+                .attr("x", function(d) { return x(d.data.Edad); })
+                .attr("y", function(d) { return y(0); })
+                .attr("height", function(d) { return 0; })
+                .attr("width",x.bandwidth())
+                .transition()
+                .duration(2000)
+                .attr("y", function(d) { return y(d[1]); })
+                .attr("height", function(d) { return y(d[0]) - y(d[1]); });
 
+            chart2.selectAll('.rect-2')
+                .attr("x", function(d) { return x(d.data.Edad); })
+                .attr("y", function(d) { return y(0); })
+                .attr("height", function(d) { return 0; })
+                .attr("width",x.bandwidth())
+                .transition()
+                .duration(2000)
+                .attr("y", function(d) { return y(d[1]); })
+                .attr("height", function(d) { return y(d[0]) - y(d[1]); });
         }
 
         /////
